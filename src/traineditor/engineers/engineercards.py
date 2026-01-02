6f2645a4004bebcd65d61799a7397722efed3a74
jeff bernardis <jeff.bernardis@gmail.com>
2026-01-01 21:13:38 -0500

    worked on loco card and engineer card formats

--- src/traineditor/engineers/engineercards.py
+++ src/traineditor/engineers/engineercards.py
@@ -20,11 +20,16 @@ class EngineerCards (Report):
 
 	def EngineerCards(self, englist):
 		css = HTML.StyleSheet()
-		css.addElement("table", {'width': '300px', 'border-spacing': '0',  'margin-left': 'auto', 'margin-right': 'auto', "font-family": '"Times New Roman", Times, serif', "font-size": "16px"})
+		css.addElement("table", {'width': '700px', 'border-spacing': '0',  'margin-left': 'auto', 'margin-right': 'auto', "font-family": '"Times New Roman", Times, serif', "font-size": "16px"})
 		css.addElement("table, th, td", {'border-collapse': 'collapse'})
 		css.addElement("th", {'text-align': 'center',  'overflow': 'hidden'})
-		css.addElement("td.eng", {"text-align": "center", "width": "170px", "font-size": "28px", "font-weight": "bold"})
-		css.addElement("td.qr", {"width": "170px", "text-align": "center",  "vertical-align": "middle"})
+		css.addElement("td.eng", {"text-align": "center", "width": "140px", "font-size": "28px", "font-weight": "bold", "border-top": "1px solid black", "border-bottom": "1px solid black", "border-left": "1px solid black"})
+		css.addElement("td.qr", {"width": "100px", "text-align": "center",  "vertical-align": "middle", "border-top": "1px solid black", "border-bottom": "1px solid black", "border-right": "1px solid black"})
+		css.addElement("td.engmt", {"text-align": "center", "width": "140px", "font-size": "28px", "font-weight": "bold"})
+		css.addElement("td.qrmt", {"width": "100px", "text-align": "center",  "vertical-align": "middle"})
+		css.addElement("td.spacer", {"width": "80px"})
+
+
 
 		html  = HTML.starthtml()
 		html += HTML.head(HTML.style({'type': "text/css", 'media': "screen, print"}, css))
@@ -33,16 +38,34 @@ class EngineerCards (Report):
 
 		rows = []
 		engOrder  = sorted(englist)
-		for eng in engOrder:
+		for ex in range(0, len(englist), 2):
+			eng = englist[ex]
 			fn = os.path.join("qrcodes", "engineer_%s.png" % eng)
 			if os.path.exists(fn):
 				img = HTML.img({"src": fn})
-				rows.append(HTML.tr({},
-						HTML.td({"class": "eng"}, eng),
-						HTML.td({"class": "qr"}, img)
-						)
-				)
-		html += HTML.table({}, "".join(rows))
+				left = (HTML.td({"class": "eng"}, eng) +
+						HTML.td({"class": "qr"}, img))
+			else:
+				left = (HTML.td({"class": "eng"}, eng) +
+						HTML.td({"class": "qr"}, ""))
+
+			if ex == len(englist)-1:
+				right = (HTML.td({"class": "engmt"}, "") +
+						HTML.td({"class": "qrmt"}, ""))
+			else:
+				eng = englist[ex+1]
+				fn = os.path.join("qrcodes", "engineer_%s.png" % eng)
+				if os.path.exists(fn):
+					img = HTML.img({"src": fn})
+					right = (HTML.td({"class": "eng"}, eng) +
+							HTML.td({"class": "qr"}, img))
+				else:
+					right = (HTML.td({"class": "eng"}, eng) +
+							HTML.td({"class": "qr"}, ""))
+
+			html += HTML.table({}, left + HTML.td({"class": "spacer"}, "") + right)
+
+			html += HTML.p({}, "")
 
 		html += HTML.endbody()
 		html += HTML.endhtml()
--- src/traineditor/locomotives/lococards.py
+++ src/traineditor/locomotives/lococards.py
@@ -29,29 +29,34 @@ class LocoCards (Report):
 
 	def LocoCards(self, locos):
 		css = HTML.StyleSheet()
-		css.addElement("table", {'width': '380px', 'border-spacing': '0',  'margin-left': 'auto', 'margin-right': 'auto', "font-family": '"Times New Roman", Times, serif', "font-size": "16px"})
+		css.addElement("table", {'width': '5in', 'height': '3in', 'border-spacing': '0',  'margin-left': 'auto', 'margin-right': 'auto', "font-family": '"Times New Roman", Times, serif', "font-size": "16px"})
 		css.addElement("table, th, td", {'border-collapse': 'collapse'})
 		css.addElement("th", {'text-align': 'center',  'overflow': 'hidden'})
-		css.addElement("td.loco", {"text-align": "center", "width": "170px", "font-size": "28px", "font-weight": "bold"})
-		css.addElement("td.desc", {"text-align": "center", "width": "170px", "font-size": "16px", "font-weight": "bold"})
-		css.addElement("td.qr", {"width": "170px", "text-align": "center",  "vertical-align": "middle"})
-		css.addElement("td.picture", {"width": "200px", "text-align": "center",  "vertical-align": "middle"})
+		css.addElement("td.loco", {"text-align": "center", "width": "35%", "font-size": "28px", "font-weight": "bold"})
+		css.addElement("td.desc", {"text-align": "center", "width": "35%", "font-size": "16px", "font-weight": "bold"})
+		css.addElement("td.qr", {"width": "35%", "text-align": "center",  "vertical-align": "middle"})
+		css.addElement("td.picture", {"width": "65%", "text-align": "center",  "vertical-align": "middle"})
+		# "@media print"
+		css.addElement(".page, .page-break", {"break-after": "page"})
+		css.addElement("div.topofpage", {"margin-bottom": "1in"})
 
 		html  = HTML.starthtml()
 		html += HTML.head(HTML.style({'type': "text/css", 'media': "screen, print"}, css))
 		
 		html += HTML.startbody()
 
-		rows = []
 		locoOrder  = sorted(locos.keys(), key=lambda x: int(x))
+		pagect = 0
+		html += HTML.div({"class": "topofpage"}, "")
 		for lid in locoOrder:
+			rows = []
 			fn = os.path.join("qrcodes", "locomotive_%s.png" % lid)
 			img = HTML.img({"src": fn})
 			fn = os.path.join("qrcodes", "picture_%s.png" % lid)
 			if os.path.isfile(fn):
 				pic = HTML.img({"src": fn})
 			else:
-				pic = "pic"
+				pic = ""
 
 			rows.append(HTML.tr({},
 					HTML.td({"class": "loco"}, lid),
@@ -66,7 +71,15 @@ class LocoCards (Report):
 					HTML.td({"class": "desc", "colspan": 2}, locos[lid]["desc"])
 					)
 			)
-		html += HTML.table({}, "".join(rows))
+			html += HTML.table({}, "".join(rows))
+			html += HTML.p({}, "")
+
+			pagect += 1
+			if pagect == 3:
+				# do a page break here
+				html += HTML.div({"class": "page-break"}, "")
+				html += HTML.div({"class": "topofpage"}, "")
+				pagect = 0
 
 		html += HTML.endbody()
 		html += HTML.endhtml()
